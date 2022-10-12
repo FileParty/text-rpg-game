7fed4c40af4de2534e0683529c9b128768b76c93
Kim in sool <pdg300@naver.com>
2022-10-12 23:21:52 +0900

    #2022.10.12 프로젝트 기본세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './css/main.scss';
 
 function App() {
+
+  const [ loginStat, setLoginStat] = useState(false);
+
+  useEffect(() => {
+    if( loginStat ) {
+
+    } else {
+
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        텍스트 RPG 게임
       </header>
+      <BrowserRouter>
+        <Routes>
+          {/* 메인화면 */}
+          <Route path="/"></Route>
+          {/* 메인화면/캐릭터선택 */}
+          <Route path="/character"></Route>
+          {/* 메인화면/게임설명 */}
+          <Route path="/description"></Route>
+          {/* 메인화면/캐릭터선택/마을 */}
+          <Route path="/game/town"></Route>
+          {/* 메인화면/캐릭터선택/마을/의뢰소 */}
+          <Route path="/game/town/requestoffice"></Route>
+          {/* 메인화면/캐릭터선택/마을/상점 */}
+          <Route path="/game/town/shop"></Route>
+          {/* 메인화면/캐릭터선택/마을/여관 */}
+          <Route path="/game/town/motel"></Route>
+          {/* 메인화면/캐릭터선택/필드 */}
+          <Route path="/game/field"></Route>
+          <Route path="*">페이지를 찾을 수 없습니다.</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
